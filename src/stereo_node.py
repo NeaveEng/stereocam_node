40cdd583b71821aa3b376228f7ea8a9370c90b40
Keegan Neave <keegan@neave.engineering>
2022-10-16 18:43:43 +0100

    cv2 stereo publisher working

--- src/stereo_node.py
+++ src/stereo_node.py
@@ -14,6 +14,7 @@ import yaml
 import io
 import signal  # for ctrl-C handling
 import sys
+import threading
 
 
 def parse_calibration_yaml(calib_file):
@@ -39,10 +40,14 @@ target_FPS = 15
 
 # initialize the camera
 print("Init capture...")
-cap = cv2.VideoCapture(1)
+cap_right = cv2.VideoCapture(0)
+cap_left = cv2.VideoCapture(1)
 
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+cap_left.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+cap_left.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+
+cap_right.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+cap_right.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 
 stream = io.BytesIO()
 
@@ -65,14 +70,14 @@ left_img_msg.height = res_y
 left_img_msg.width = res_x
 left_img_msg.step = res_x * 3  # bytes per row: pixels * channels * bytes per channel (1 normally)
 left_img_msg.encoding = 'rgb8'
-left_img_msg.header.frame_id = 'stereo_camera'  # TF frame
+left_img_msg.header.frame_id = 'left_camera'  # TF frame
 
 right_img_msg = Image()
 right_img_msg.height = res_y
 right_img_msg.width = res_x
 right_img_msg.step = res_x * 3
 right_img_msg.encoding = 'rgb8'
-right_img_msg.header.frame_id = 'stereo_camera'
+right_img_msg.header.frame_id = 'right_camera'
 
 imageBytes = res_x * res_y * 3
 
@@ -85,10 +90,11 @@ right_cam_info = parse_calibration_yaml('right.yaml')
 # this is supposed to shut down gracefully on CTRL-C but doesn't quite work:
 def signal_handler(signal, frame):
     print('CTRL-C caught')
-    print('closing camera')
-    cap.close()
+    print('closing cameras')
+    cap_left.release()
+    cap_right.release()
     time.sleep(1)
-    print('camera closed')
+    print('cameras closed')
     sys.exit(0)
 
 
@@ -97,59 +103,56 @@ signal.signal(signal.SIGINT, signal_handler)
 # -----------------------------------------------------------
 
 print("Setup done, entering main loop")
-framecount = 0
-frametimer = time.time()
-toggle = True
+
 # capture frames from the camera
 br = CvBridge()
 
 debug_view = False
 
-try:
-    print("Stream opened: ", cap.isOpened())
+def Publish(cap, left):
+    global left_img_msg, left_cam_info, left_cam_pub, right_img_msg, right_cam_info, right_cam_pub, br
+    try:
+        while(cap.isOpened()):
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+            if ret == True:
+                if(debug_view == True):
+                    # Display the resulting frame
+                    cv2.imshow('Source',frame)
 
-    while(cap.isOpened()):
-        # Capture frame-by-frame
-        ret, frame = cap.read()
-        if ret == True:
-            if(debug_view == True):
-                # Display the resulting frame
-                cv2.imshow('Source',frame)
+                # Press Q on keyboard to  exit
+                if cv2.waitKey(25) & 0xFF == ord('q'):
+                    break
 
-            # Press Q on keyboard to  exit
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                break
+                stamp = rospy.Time.now()
 
-            framecount += 1
-
-            stamp = rospy.Time.now()
-            left_img_msg.header.stamp = stamp
-            right_img_msg.header.stamp = stamp
-            left_cam_info.header.stamp = stamp
-            right_cam_info.header.stamp = stamp
-            
-            left_cam_pub.publish(left_cam_info)
-            right_cam_pub.publish(right_cam_info)
-            
-
-            if(debug_view == True):
-                cv2.imshow('Left', frame[0:240, 0:320])
-                cv2.imshow('Right', frame[240:480, 0:320])
-
-            # publish the image pair
-            left_img_pub.publish(br.cv2_to_imgmsg(frame, encoding="bgr8"))
-            # right_img_pub.publish(br.cv2_to_imgmsg(frame[240:480, 0:320], encoding="bgr8"))
-
-            # console info
-            if time.time() > frametimer + 1.0:
-                if toggle:
-                    indicator = '  o'  # just so it's obviously alive if values aren't changing
+                if(left):
+                    left_img_msg.header.stamp = stamp
+                    left_cam_info.header.stamp = stamp
+                    left_cam_pub.publish(left_cam_info)
+                    left_img_pub.publish(br.cv2_to_imgmsg(frame, encoding="bgr8"))
                 else:
-                    indicator = '  -'
-                toggle = not toggle
-                print
-                'approx publish rate:', framecount, 'target FPS:', target_FPS, indicator
-                frametimer = time.time()
-                framecount = 0
-except:
-    print(sys.exc_info()[0])
+                    right_img_msg.header.stamp = stamp
+                    right_cam_info.header.stamp = stamp            
+                    right_cam_pub.publish(right_cam_info)
+                    right_img_pub.publish(br.cv2_to_imgmsg(frame, encoding="bgr8"))
+        
+    except:
+        print(sys.exc_info()[0])
+
+def PublishLeft():
+    global cap_left
+    Publish(cap_left, True)
+
+def PublishRight():
+    global cap_right
+    Publish(cap_right, False)
+    
+trdLeft = threading.Thread(target=PublishLeft)
+trdRight = threading.Thread(target=PublishRight)
+
+trdLeft.start()
+trdRight.start()
+
+trdLeft.join()
+trdRight.join()
